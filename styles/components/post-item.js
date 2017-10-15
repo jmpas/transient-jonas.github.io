bfdec0d72b87c5b8097f4256a6aa8f06d8dfb9c2
nipher <jonas@nipher.io>
2017-10-16 00:56:23 +0200

    Add media queries for post items

--- styles/components/post-item.js
+++ styles/components/post-item.js
@@ -6,6 +6,36 @@ export default `
   margin-bottom: 10px;
 }
 
+@media (min-width: 1100px) and (max-width: 1190px) {
+  .post-item {
+    width: 450px;
+  }
+}
+
+@media (min-width: 1000px) and (max-width: 1100px) {
+  .post-item {
+    width: 400px;
+  }
+}
+
+@media (min-width: 900px) and (max-width: 1000px) {
+  .post-item {
+    width: 350px;
+  }
+}
+
+@media (min-width: 780px) and (max-width: 840px) {
+  .post-item {
+    width: 300px;
+  }
+}
+
+@media (max-width: 780px) {
+  .post-item {
+    width: 100%;
+  }
+}
+
 .post-content {
   flex-basis: 65%;
   padding: 0 0 0 5px;
