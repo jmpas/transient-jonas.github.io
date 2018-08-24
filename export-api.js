b3d6f5a306d3cbe28c93ec0df77d5cd3618964db
nipher <jonas@nipher.io>
2018-08-24 21:35:48 +0200

    Add export api

---
+++ export-api.js
@@ -0,0 +1,54 @@
+const { writeFile } = require('fs')
+const { promisify } = require('util');
+const fetch = require('isomorphic-fetch')
+const { spawn } = require('child_process')
+spawn('mkdir', ['-p', 'out/api/post'])
+
+const { getPostList, getMarkdownFrom, formatDate, sortPosts } = require('./helpers/post')
+const { getExperimentsList } = require('./helpers/experiment')
+const write = promisify(writeFile)
+
+let postsList = null
+let server = null
+
+async function getPostsList() {
+  if (postsList) return postsList
+
+  try {
+    const res = await fetch('http://localhost:3000/api/posts.json')
+    return postsList = (await res.json()).posts
+  } catch (err) {
+    console.error('Make sure to have the backend running on localhost:3000')
+    process.exit(1)
+  }
+}
+
+async function exportPostApi() {
+  const postsList = await getPostsList()
+
+  const promises = postsList.map(async ({ slug }) => {
+    const res = await fetch(`http://localhost:3000/api/post/${slug}`)
+    const post = await res.text()
+
+    await write(`./out/api/post/${slug}.json`, post)
+  })
+
+  return Promise.all(promises)
+}
+
+async function exportPostsApi() {
+  const postsList = await getPostsList()
+  await write(`./out/api/posts.json`, JSON.stringify({ posts: postsList }))
+}
+
+async function exportExperimentsApi() {
+  const experiments = await getExperimentsList()
+  await write(`./out/api/experiments.json`, JSON.stringify({ experiments }))
+}
+
+module.exports = Promise.all([
+  exportPostApi().then(() => console.log('API endpoint exported: /api/post/<slug>.json')),
+  exportPostsApi().then(() => console.log('API endpoint exported: /api/posts.json')),
+  exportExperimentsApi().then(() => console.log('API endpoint exported: /api/experiments.json'))
+])
+
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPlugins = require('next-compose-plugins')
 const bundleAnalyzer = require("@zeit/next-bundle-analyzer")
 const preact = require('@zeit/next-preact')
+const { getExperimentsList } = require('./helpers/experiment')
 
 module.exports = withPlugins([
 	[bundleAnalyzer, {
@@ -18,4 +19,63 @@ module.exports = withPlugins([
 		}
 	}],
 	preact
-])
+], {
+  async exportPathMap() {
+    if (process.env.NODE_ENV !== 'production') return {}
+    await require('./export-api')
+    const { posts: postsList } = require('./out/api/posts')
+    const experimentsList = await getExperimentsList()
+
+    const experiments = experimentsList.reduce((acc, experiment) => (
+      {
+        ...acc,
+        [`/experiment/${experiment.slug}`]: {
+          page: `/experiment/${experiment.slug}`,
+          query: { ...experiment, build: true }
+        }
+      }
+    ), {})
+
+    const posts = postsList.reduce((pages, data) => {
+      const post = require(`./out/api/post/${data.slug}`)
+
+      return {
+        ...pages,
+        [`/${data.slug}`]: {
+          page: '/post',
+          query: { ...post, build: true }
+        }
+      }
+    }, {})
+
+    const pages = {
+      '/': {
+        page: '/',
+        query: { build: true }
+      },
+      '/blog': {
+        page: '/blog',
+        query: { posts: postsList, build: true }
+      },
+      '/photography': {
+        page: '/photography',
+        query: { build: true }
+      },
+      '/experiments': {
+        page: '/experiments',
+        query: { experiments: experimentsList, build: true }
+      },
+      '/about': {
+        page: '/about',
+        query: { build: true }
+      }
+    }
+
+    // combine the map of post pages with landing pages
+    return {
+      ...experiments,
+      ...posts,
+      ...pages
+    }
+  }
+})
